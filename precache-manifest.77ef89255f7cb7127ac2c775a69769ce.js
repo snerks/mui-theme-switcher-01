3fc0e57b27c1e909947c1b57d247cf217cc089a7
snerks <martin.github@outlook.com>
2020-04-15 12:53:02 +0100

    Updates

---
+++ precache-manifest.77ef89255f7cb7127ac2c775a69769ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b850c9346389aa013a93be948c16a43",
+    "url": "/mui-theme-switcher-01/index.html"
+  },
+  {
+    "revision": "84e540fd0e1355898453",
+    "url": "/mui-theme-switcher-01/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2099d544a6a356ab565b",
+    "url": "/mui-theme-switcher-01/static/js/2.9d27a686.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mui-theme-switcher-01/static/js/2.9d27a686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e540fd0e1355898453",
+    "url": "/mui-theme-switcher-01/static/js/main.d44b6f4c.chunk.js"
+  },
+  {
+    "revision": "3a4c9495c1b8bcec87c1",
+    "url": "/mui-theme-switcher-01/static/js/runtime-main.4d7476e2.js"
+  }
+]);
